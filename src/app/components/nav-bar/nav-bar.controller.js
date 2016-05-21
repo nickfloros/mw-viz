401e9c90046f1f3f3f7bd138977778577204f4e9
Nick Floros <nickfloros@gmail.comt>
2016-05-21 12:02:48 +0100

    fixing search box

--- src/app/components/nav-bar/nav-bar.controller.js
+++ src/app/components/nav-bar/nav-bar.controller.js
@@ -3,31 +3,42 @@ var _ = require('lodash');
 
 module.exports = angular.module('nav-panel-controller-module', [
 	require('../../services/mapping.service.js').name
-	]).controller('navBarController',['MappingService', function(MappingSerivce){
+]).controller('navBarController', ['MappingService', function (MappingSerivce) {
 
-		var ctrl = this,
-			_dataSet,
-			_dataSets = ['Marker posts', 'SOS Boxes', 'Junctions', 'Service Stations'];
+	var ctrl = this,
+		_dataSet = '',
+		_searchTerm = '',
+		_dataSets = ['Marker posts', 'SOS Boxes', 'Junctions', 'Service Stations'];
 
-		_.extend(ctrl,{
-			clearMap : function() {
-				MappingSerivce.reset();
-			},
-			dataSet : function (val) {
-				if (arguments.length) {
-					_dataSet = val;
-					console.log(val);
-				}
-				return arguments.length? _dataSet = val : _dataSet;
-			},
-			options : function() {
-				return _dataSets;
-			},
-			isSelected : function (option) {
-				return _dataSet === option;
-			},
-			modelOptions : {
-				getterSetter: true
+	_.extend(ctrl, {
+		clearMap: function () {
+			MappingSerivce.reset();
+		},
+		dataSet: function (val) {
+			if (arguments.length) {
+				_dataSet = val;
+				_searchTerm = '';
+				console.log(val);
 			}
-		});
-	}]);
+			return arguments.length ? _dataSet = val : _dataSet;
+		},
+		searchTerm: function (val) {
+			return arguments.length ? _searchTerm = val : _searchTerm;
+		},
+		search: function () {
+			console.log('searching ... ', _searchTerm);
+		},
+		searchTermPlaceHolder: function () {
+			return _.isEmpty(_dataSet) ? 'Plase select data set' : _dataSet + ' search for ..';
+		},
+		options: function () {
+			return _dataSets;
+		},
+		isSelected: function (option) {
+			return _dataSet === option;
+		},
+		modelOptions: {
+			getterSetter: true
+		}
+	});
+}]);
